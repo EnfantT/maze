d288c64ada18ad3a3502d6dc6d057b51f7e2777c
Sung-jae Park <nicesj.park@samsung.com>
2016-11-28 11:32:52 +0900

    Remove unnecessary methods.
    Clean up code
    
    Signed-off-by: Sung-jae Park <nicesj.park@samsung.com>

--- maze/maze/CView.cpp
+++ maze/maze/CView.cpp
@@ -45,42 +45,6 @@ void CView::Destroy(void)
 	delete this;
 }
 
-void CView::SetEye(vec3 &eye)
-{
-	m_eye = eye;
-	m_updated = true;
-}
-
-void CView::SetEye(float x, float y, float z)
-{
-	m_eye.set(x, y, z);
-	m_updated = true;
-}
-
-void CView::SetAt(vec3 &at)
-{
-	m_at = at;
-	m_updated = true;
-}
-
-void CView::SetAt(float x, float y, float z)
-{
-	m_at.set(x, y, z);
-	m_updated = true;
-}
-
-void CView::SetUp(vec3 &up)
-{
-	m_up = up;
-	m_updated = true;
-}
-
-void CView::SetUp(float x, float y, float z)
-{
-	m_up.set(x, y, z);
-	m_updated = true;
-}
-
 mat4 CView::Matrix(void)
 {
 	if (m_updated) {
@@ -89,21 +53,6 @@ mat4 CView::Matrix(void)
 	}
 
 	return m_viewMatrix;
-}
-
-vec3 CView::Eye(void)
-{
-	return m_eye;
-}
-
-vec3 CView::At(void)
-{
-	return m_at;
-}
-
-vec3 CView::Up(void)
-{
-	return m_up;
 }
 
 bool CView::Updated(void)
--- maze/maze/CView.h
+++ maze/maze/CView.h
@@ -1,3 +1,9 @@
+/**
+ * \brief
+ * This class is used for manipulating Camera(aka. Eye)
+ * Coordinate system of the camera is inverted of the world's one.
+ */
+
 #pragma once
 #if !defined(__CVIEW_H)
 #define __CVIEW_H
@@ -26,19 +32,6 @@ private:
 public:
 	static CView *GetInstance(void);
 	void Destroy(void);
-
-	void SetEye(vec3 &eye);
-	void SetEye(float x, float y, float z);
-
-	void SetAt(vec3 &at);
-	void SetAt(float x, float y, float z);
-
-	void SetUp(vec3 &up);
-	void SetUp(float x, float y, float z);
-
-	vec3 Eye(void);
-	vec3 At(void);
-	vec3 Up(void);
 
 	mat4 Matrix(void);
 
